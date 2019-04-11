cad206f982068c218eaa0e1dd89e8baf61df6633
guoshuyu <wdcszyyx@outlook.com>
2019-04-11 22:34:34 +0800

    simple edition

--- Tree/Tree.cpp
+++ Tree/Tree.cpp
@@ -3,12 +3,65 @@
 
 #include "pch.h"
 #include <iostream>
+#include <stdio.h>
+#include <filesystem>
+#include <io.h>
+#include <fstream>
+#include <string>
+using namespace std;
+//void tree(string path, vector<string>& files,int);
+void tree(string path, vector<string>& files, int depth);
+#include <iomanip>
+#include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
-}
+	char filePath[] = "E:\\myKE";
+	vector<string> files;
+	char distAll[] = "AllFiles.txt";
+	tree(filePath, files,2);
+	//ostream mos(distAll);
+	//int size = files.size();
+	//cout << size << endl;
+	//for (int i = 0; i < size; i++) {
+	//	cout << files[i] << endl;
 
+	//}
+	//ofn.close();
+	//std::cout << "Hello World!\n";
+	getchar();
+	return 0;
+}
+void tree(string path, vector<string>& files,int depth) {
+	long hFile = 0;
+	//文件信息
+	struct _finddata_t fileinfo;
+	string p;
+	if ((hFile = _findfirst(p.assign(path).append("\\*").c_str(), &fileinfo)) != -1) {
+		do {
+			if ((fileinfo.attrib & _A_SUBDIR)) {
+				if (strcmp(fileinfo.name, ".") != 0 && strcmp(fileinfo.name, "..") != 0) {
+					//files.push_back(p.assign(path).append("\\").append(fileinfo.name));
+					cout << setfill(' ')<<right<< setw(depth * 4 - 7) << ""<<setfill('-') << right << setw(4) << "" << (fileinfo.name) << "\\"<<endl;
+					tree(p.assign(path).append("\\").append(fileinfo.name), files ,depth+1);
+				}
+
+			}
+			else {
+				//cout << right << setw(depth * 4-1) <<""<< (fileinfo.name)<<endl;
+				cout << setfill(' ') << right << setw(depth * 4 - 7) << "" << setfill('-') << right << setw(4) << "" << (fileinfo.name) << "\\" << endl;
+				//files.push_back(p.assign(path).append("\\").append(fileinfo.name));
+			
+			}
+
+		} while (_findnext(hFile, &fileinfo) == 0);
+		_findclose(hFile);
+
+	}
+
+}
+void print_has_before_blanks();
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
